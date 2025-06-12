a5d855ae361ff9ef4ed7a0ab8d5e037c0f833d95
Vamshi Thallapalli <127075912+vamshithallapalli@users.noreply.github.com>
2025-06-12 11:48:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,28 @@ const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
   // Write your code here
+	
+	let result = [];
+	let currArr = [];
+	let currSum = 0;
+
+	for(let num: arr){
+
+		if(currSum+num > n){
+			result.push(currArr);
+			currArr = [num];
+			currSum = num;
+		}
+		else{
+		currArr.push(num);
+		currSum += num;
+		}
+	}
+
+    if(currArr.length > 0){
+		result.push(currArr);
+	}
+return result;
 };
 
 const n = prompt("Enter n: ");
